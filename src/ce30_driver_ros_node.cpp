70994539a66c49d08272809e02ba1b6a6592cbaf
Martin Appo <martin.appo@cleveron.eu>
2019-05-31 10:16:38 +0300

    chore: reformat code with clion, update package maintainer, change ip

--- src/ce30_driver_ros_node.cpp
+++ src/ce30_driver_ros_node.cpp
@@ -9,34 +9,34 @@ ros::Publisher gPub;
 std::string gFrameID = "ce30";
 
 void DataReceiveCB(shared_ptr<PointCloud> cloud) {
-  sensor_msgs::PointCloud pointcloud;
-  pointcloud.header.stamp = ros::Time::now();
-  pointcloud.header.frame_id = gFrameID;
-  static int point_num = 320 * 20;
-  pointcloud.points.reserve(point_num);
-  for (auto& point : cloud->points) {
-    static geometry_msgs::Point32 ros_point;
-    ros_point.x = point.x;
-    ros_point.y = point.y;
-    ros_point.z = point.z;
-    pointcloud.points.push_back(ros_point);
-  }
-  if (gPub.getNumSubscribers() > 0) {
-    gPub.publish(pointcloud);
-  }
+    sensor_msgs::PointCloud pointcloud;
+    pointcloud.header.stamp = ros::Time::now();
+    pointcloud.header.frame_id = gFrameID;
+    static int point_num = 320 * 20;
+    pointcloud.points.reserve(point_num);
+    for (auto &point : cloud->points) {
+        static geometry_msgs::Point32 ros_point;
+        ros_point.x = point.x;
+        ros_point.y = point.y;
+        ros_point.z = point.z;
+        pointcloud.points.push_back(ros_point);
+    }
+    if (gPub.getNumSubscribers() > 0) {
+        gPub.publish(pointcloud);
+    }
 }
 
-int main(int argc, char** argv) {
-  ros::init(argc, argv, "ce30_node");
-  ros::NodeHandle nh;
-  nh.param<std::string>("frame_id", gFrameID, gFrameID);
-  gPub = nh.advertise<sensor_msgs::PointCloud>("ce30_points", 1);
-  UDPServer server;
-  server.RegisterCallback(DataReceiveCB);
-  if (!server.Start()) {
-    return -1;
-  }
-  while (ros::ok()) {
-   	server.SpinOnce();
-  }
+int main(int argc, char **argv) {
+    ros::init(argc, argv, "ce30_node");
+    ros::NodeHandle nh;
+    nh.param<std::string>("frame_id", gFrameID, gFrameID);
+    gPub = nh.advertise<sensor_msgs::PointCloud>("ce30_points", 1);
+    UDPServer server;
+    server.RegisterCallback(DataReceiveCB);
+    if (!server.Start()) {
+        return -1;
+    }
+    while (ros::ok()) {
+        server.SpinOnce();
+    }
 }
